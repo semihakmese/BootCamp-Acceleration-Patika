621a3048d5e67037b8dcc9e2e365610e14fdb33f
semihakmese <109501114+semihakmese@users.noreply.github.com>
2022-08-18 10:38:43 +0300

    Update FlattenList.py

--- FlattenList.py
+++ FlattenList.py
@@ -7,7 +7,7 @@
 #After that we have to create a empty list to add flatten values
 #So the function is going to scan whole values and check them if,
 #is there any list object, that's gonna be converted by applying the function again.
-#If not the value is going to add to our new list.
+#If not the value is going to added to our new list.
 
 
 l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
